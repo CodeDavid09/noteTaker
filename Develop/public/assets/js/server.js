3cff19ea32fc3aa04421edce1b2223e96c4f1020
David Gavino <ggavino2@uccs.edu>
2022-01-17 14:03:32 -0700

    modifies .js files

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -6,25 +6,24 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3001; 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/db', 'dbjson'));
+    res.sendFile(path.join(__dirname, './Develop/db', 'db.json'));
 });
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public', 'notes.html'));
 }); 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-
 app.post("/api/notes", (req, res) => {
     let newNote = req.body;
     let noteList = JSON.parse(fs.readFileSync("./Develop/db.json", "utf8"));
     let notelength = (noteList.length).toString();
 
-    //creates new id based on length and assign it to each json object
+    //creates new id and assigns it to each json object
     // .push function to push new note to db.json
     newNote.id = notelength;
     noteList.push(newNote);
